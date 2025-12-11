2553a01ca82655f881118055b24a9b71b4854a2f
CalebShufford <cshufford@my.yearupunited.org>
2025-12-11 13:56:45 -0500

    Added the ProductsController

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
@@ -0,0 +1,34 @@
+package com.pluralsight.NorthwindTradersAPI.controllers;
+
+import com.pluralsight.NorthwindTradersAPI.dao.ProductDao;
+import com.pluralsight.NorthwindTradersAPI.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    private final ProductDao productDao;
+
+    @Autowired
+
+    public ProductsController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @GetMapping
+    public List<Product>getAllProducts(){
+        return productDao.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id){
+        return productDao.getById(id);
+    }
+}
