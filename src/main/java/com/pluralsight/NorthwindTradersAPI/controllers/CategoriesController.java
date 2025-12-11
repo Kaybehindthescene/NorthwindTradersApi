d8e2beab2d84d3551bc9ceb1f4fddce45e2a737b
CalebShufford <cshufford@my.yearupunited.org>
2025-12-11 14:02:42 -0500

    Added the CategoriesController

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/CategoriesController.java
@@ -0,0 +1,33 @@
+package com.pluralsight.NorthwindTradersAPI.controllers;
+
+import com.pluralsight.NorthwindTradersAPI.dao.CategoryDao;
+import com.pluralsight.NorthwindTradersAPI.models.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoriesController {
+
+    private final CategoryDao categoryDao;
+
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    @GetMapping
+    public List<Category>getAllCategories(){
+        return categoryDao.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable int id) {
+        return categoryDao.getById(id);
+    }
+}
